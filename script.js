e9cdaabd4ed0900d15692e81ef41aee625579cbb
shadow22805 <65177536+shadow22805@users.noreply.github.com>
2020-05-12 10:00:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "static/images/otto.jpg";
+    div.appendChild(image);
+}
